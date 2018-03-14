b2f96f5790afe4515feb317174a95851f5ae73c4
mirelatv <noelia.mirela@gmail.com>
2018-03-14 10:06:29 -0500

    descomentado

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,24 +1,24 @@
-// import React, { Component } from 'react';
-// import Texto from './Lista'
+import React, { Component } from 'react';
+import Texto from './Lista'
 
 
 
 
-// const Section = ()=>{
+const Section = ()=>{
 
-// 		return (
-// 			<div className="App AboutMe ">						
-// 					<div>
-//             <p>Soy  Noelia  Tello ,  estoy feliz de aprender cosas  nuevas,   se que  aun  hay mucho 
-//                por aprender  y bastante por parcticar  pero teng el deso  y las  ganas  de  hacerlo </p>
-// 					<div>
-//           	<Lista/>
-//             </div>
-// 					</div>
+		return (
+			<div className="App AboutMe ">						
+					<div>
+            <p>Soy  Noelia  Tello ,  estoy feliz de aprender cosas  nuevas,   se que  aun  hay mucho 
+               por aprender  y bastante por parcticar  pero teng el deso  y las  ganas  de  hacerlo </p>
+					<div>
+          	<Lista/>
+            </div>
+					</div>
 				
-// 				</div>
+				</div>
 			
-// 		);
-// 	}
+		);
+	}
 
-// export default Section;
+export default Section;
